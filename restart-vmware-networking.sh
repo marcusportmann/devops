ab79ff664e71293b94f7b1599d22e1f64a56595e
Marcus Portmann <marcus@mmp.guru>
2020-06-18 14:43:00 +0200

    Kubernetes fixes

--- restart-vmware-networking.sh
+++ restart-vmware-networking.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+rm -f /opt/vagrant-vmware-desktop/settings/nat.json
+killall vagrant-vmware-utility
 /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --configure
 /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --stop
+rm -f /Library/Preferences/VMware\ Fusion/vmnet1/*.bak
+rm -f /Library/Preferences/VMware\ Fusion/vmnet8/*.bak
+rm -f /Library/Preferences/VMware\ Fusion/networking.bak*
 /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --start
